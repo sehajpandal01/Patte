0a52b9c113fb45147234e34e83f38dd55f3a836e
sehajpandal01 <36576068+sehajpandal01@users.noreply.github.com>
2026-01-21 22:58:51 -0800

    Remove extra newline in Deck.swift

--- Deck.swift
+++ Deck.swift
@@ -4,7 +4,6 @@ class Deck {
     init() {
         reset()
     }
-
     func reset() {
         let suits = ["♠", "♥", "♦", "♣"]
         let ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
