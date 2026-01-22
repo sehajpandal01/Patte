03b1ae05af08d4db1baa6af65d8d6c3de133c572
sehajpandal01 <36576068+sehajpandal01@users.noreply.github.com>
2026-01-22 00:13:19 -0800

    Remove unnecessary newline in Player.swift

--- Player.swift
+++ Player.swift
@@ -4,7 +4,6 @@ struct Player: Identifiable {
     let name: String
     var hand: [Card]
     var collectedCards: [Card] = []
-
     mutating func playCard(_ card: Card) {
         hand.removeAll { $0 == card }
     }
